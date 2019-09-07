cb071dac65894ad9d7d1d456c9ba90bee2acd626
jfox (Professor Emeritus ) <jfox@mcmaster.ca>
2019-09-07 01:07:53 +0000

    finish documenting changes in code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/norm@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/norm/R/startup.R
+++ pkg/norm/R/startup.R
@@ -1,3 +1,5 @@
+# Added by J. Fox 2019-09-06
+
 .onAttach <- function(...){
     packageStartupMessage(gettext(paste(c("This package has some major limitations", 
          "(for example, it does not work reliably when",
